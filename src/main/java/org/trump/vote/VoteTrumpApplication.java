bb48c9f24a1e54b5ae6b0440c8b44db0d8e83217
Liu Jun <liujun@cinaval.com>
2024-10-23 10:30:57 +0800

    init

---
+++ src/main/java/org/trump/vote/VoteTrumpApplication.java
@@ -0,0 +1,46 @@
+package org.trump.vote;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Properties;
+
+@SpringBootApplication
+@Slf4j
+public class VoteTrumpApplication implements CommandLineRunner {
+
+    @Value("${server.port}")
+    private int port;
+
+    @Value("${spring.application.name}")
+    private String applicationName;
+
+    public static void main(String[] args) {
+        SpringApplication.run(VoteTrumpApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        try {
+            new Thread(() -> {
+                Properties p = System.getProperties();
+                String pid = p.getProperty("PID");
+                String userCountry = p.getProperty("user.country");
+                String userLanguage = p.getProperty("user.language");
+                String timezone = p.getProperty("user.timezone");
+                String fileEncoding = p.getProperty("file.encoding");
+                String tomcatHome = p.getProperty("catalina.home");
+                String javaHome = p.getProperty("java.home");
+                String javaVersion = p.getProperty("java.version");
+
+                log.info("pid = {}, user.country = {}, user.language = {}, timezone = {}, file.encoding = {}, catalina.home = {}, java.home = {}, java.version = {}, application name = {}, port = {}", pid, userCountry, userLanguage, timezone, fileEncoding, tomcatHome, javaHome, javaVersion, applicationName, port);
+            }).start();
+        } catch (Exception e) {
+            log.error("Application = {} start failed. error = {}", applicationName, e.getCause().getMessage());
+            throw new Exception(e);
+        }
+    }
+}
